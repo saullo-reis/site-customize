d625e47e4d72a43a564718aa988e008959e13a38
Saullo Reis <saulloreis01@hotmail.com>
2023-03-15 11:52:57 -0300

    adicionando um userSelector para o utilizador ver seu nome nos créditos

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,9 +1,13 @@
 import "./footer.sass";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
 export const Footer = () => {
+const name = useSelector((state: RootState) => state.name.name)
+
   return (
     <section className="footer">
-      <p className="footer-text">Saullo Reis &copy; 2023</p>
+      <p className="footer-text">{name} &copy; 2023</p>
     </section>
   );
 };
