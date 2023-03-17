6a8bdb4d1ad9d45aa201c4b50ecb6b88aef912cd
Saullo Reis <saulloreis01@hotmail.com>
2023-03-17 11:46:24 -0300

    types separado do componente

---
+++ src/utils/utils.tsx
@@ -0,0 +1,17 @@
+export interface BoxTypes {
+  quantity?: number;
+  images?: Array<string>;
+  backgroundBox?: string;
+  texts?: Array<string>;
+}
+
+export interface TextAndImageType {
+  text?: string;
+  imagePosition?: string;
+  image?: string;
+}
+
+export interface HeaderTypes {
+  background?: string;
+  logo?: string;
+}
