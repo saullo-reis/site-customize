180c4df25e8dba2db680f23842f5e6491d02b66a
Saullo Reis <saulloreis01@hotmail.com>
2023-03-20 13:16:40 -0300

    adicionando a rota para a página criada

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,12 +1,17 @@
+import { useSelector } from "react-redux"
 import { Route, Routes } from "react-router-dom"
 import { EditHeader } from "../main/edit/edit"
 import { Main } from "../main/main"
+import { PageCreated } from "../main/page-created/page-created"
+import { RootState } from "../store"
 
 export const AppRoutes = () => {
+    const name = useSelector((state: RootState) => state.name.name)
     return(
         <Routes>
             <Route path="/" element={<Main/>}></Route>
             <Route path="/edit" element={<EditHeader/>}></Route>
+            <Route path={`/page/${name}`} element={<PageCreated/>}></Route>
         </Routes>
     )
 }
