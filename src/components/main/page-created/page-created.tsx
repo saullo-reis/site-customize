567b0bb9e3b79989c3467528f3cf37b2f8a3e276
Saullo Reis <saulloreis01@hotmail.com>
2023-03-27 10:55:44 -0300

    adicionando as condicionais para a renderização dos componentes de acordo com o layout

--- src/components/main/page-created/page-created.tsx
+++ src/components/main/page-created/page-created.tsx
@@ -1,9 +1,11 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { Box } from "./box";
-import './page.sass'
+import { Text } from "./text";
+import "./page.sass";
+import { TextAndImage } from "./text-and-image";
 
 export const PageCreated = () => {
   const layout = useSelector((state: RootState) => state.data.layout);
-  return <section className="page">{layout === "box" && <Box />}</section>;
+  return <section className="page">{layout === "box" && <Box />}{layout === 'text' && <Text/>}{layout === 'image' && <TextAndImage/>}</section>;
 };
