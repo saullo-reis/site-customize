a5dcdc339b2acb4f8945f8c58af00d88049d5eda
Saullo Reis <saulloreis01@hotmail.com>
2023-03-27 10:52:11 -0300

    adicionando a rota ao click na logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 import { RootState } from "../store";
 import { useSelector } from "react-redux/es/exports";
 import "./header.sass";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
   const name = useSelector((state: RootState) => state.name.name);
   const data = useSelector((state: RootState) => state.data.headerAndFooter)
-  console.log(data)
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/edit')
+  }
+
   return (
-    <section className="header"
+    <section className="header" onClick={() => {handleClick()}}
     style={{backgroundColor: data.background}}>
       <img
         src={data.logo}
